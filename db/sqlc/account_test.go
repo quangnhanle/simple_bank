2fb4e70d2a53a86f727019ff1f581b5bf9993076
quangnhanle <21522402@gm.uit.edu.vn>
2026-06-05 15:26:46 +0700

    fix get list test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -84,18 +84,23 @@ func TestDeleteAccount(t *testing.T) {
 }
 
 func TestListAccounts(t *testing.T) {
+	var lastAccount Account
 	for i := 0; i < 10; i++ {
-		createRandomAccount(t)
+		lastAccount = createRandomAccount(t)
 	}
 
-	accounts, err := testQueries.ListAccounts(context.Background(), ListAccountsParams{
+	arg := ListAccountsParams{
+		Owner:  lastAccount.Owner,
 		Limit:  5,
-		Offset: 5,
-	})
+		Offset: 0,
+	}
+
+	accounts, err := testStore.ListAccounts(context.Background(), arg)
 	require.NoError(t, err)
-	require.Len(t, accounts, 5)
+	require.NotEmpty(t, accounts)
 
 	for _, account := range accounts {
 		require.NotEmpty(t, account)
+		require.Equal(t, lastAccount.Owner, account.Owner)
 	}
 }
